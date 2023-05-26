5d51df64fc0780919499b389fe71d759c0f604ed
Diego Beraldin <diego.beraldin@gmail.com>
2023-05-26 08:29:50 +0200

    fix(common): fix cursor position custom text fields (#19)

--- core-common/src/jvmMain/kotlin/common/ui/components/CustomTextField.kt
+++ core-common/src/jvmMain/kotlin/common/ui/components/CustomTextField.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Dp
@@ -76,7 +75,7 @@ fun CustomTextField(
                 .padding(horizontal = Spacing.xs, vertical = Spacing.xs),
             value = textFieldValue,
             onValueChange = {
-                textFieldValue = it.copy(selection = TextRange(it.text.length))
+                textFieldValue = it
                 onValueChange(it.text)
             },
             cursorBrush = SolidColor(textColor),
