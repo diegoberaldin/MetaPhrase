9c691203ddb8c3719f62ca912b5ac49acd737dd2
Diego Beraldin <diego.beraldin@gmail.com>
2023-06-06 20:53:34 +0200

    chore(glossary): improve encapsulation

--- domain-glossary/src/jvmMain/kotlin/glossary/di/Module.kt
+++ domain-glossary/src/jvmMain/kotlin/glossary/di/Module.kt
@@ -1,11 +1,12 @@
 package glossary.di
 
+import glossary.repo.DefaultGlossaryTermRepository
 import glossary.repo.GlossaryTermRepository
 import org.koin.dsl.module
 
 val glossaryModule = module {
-    single {
-        GlossaryTermRepository(
+    single<GlossaryTermRepository> {
+        DefaultGlossaryTermRepository(
             dao = get(),
         )
     }
--- domain-glossary/src/jvmMain/kotlin/glossary/repo/GlossaryTermRepository.kt
+++ domain-glossary/src/jvmMain/kotlin/glossary/repo/GlossaryTermRepository.kt
@@ -1,27 +1,24 @@
 package glossary.repo
 
 import data.GlossaryTermModel
-import persistence.dao.GlossaryTermDao
 
-class GlossaryTermRepository(
-    private val dao: GlossaryTermDao,
-) {
-    suspend fun create(model: GlossaryTermModel) = dao.insert(model)
+interface GlossaryTermRepository {
+    suspend fun create(model: GlossaryTermModel): Int
 
-    suspend fun getById(id: Int) = dao.getById(id)
+    suspend fun getById(id: Int): GlossaryTermModel?
 
-    suspend fun get(lemma: String, lang: String) = dao.getBy(lemma = lemma, lang = lang)
+    suspend fun get(lemma: String, lang: String): GlossaryTermModel?
 
-    suspend fun update(model: GlossaryTermModel) = dao.update(model)
+    suspend fun update(model: GlossaryTermModel): Int
 
-    suspend fun delete(model: GlossaryTermModel) = dao.delete(model)
+    suspend fun delete(model: GlossaryTermModel): Int
 
-    suspend fun areAssociated(sourceId: Int, targetId: Int) =
-        dao.areAssociated(sourceId = sourceId, targetId = targetId)
+    suspend fun areAssociated(sourceId: Int, targetId: Int): Boolean
 
-    suspend fun associate(sourceId: Int, targetId: Int) = dao.associate(sourceId = sourceId, targetId = targetId)
+    suspend fun associate(sourceId: Int, targetId: Int)
 
-    suspend fun disassociate(sourceId: Int, targetId: Int) = dao.disassociate(sourceId = sourceId, targetId = targetId)
+    suspend fun disassociate(sourceId: Int, targetId: Int): Int
 
-    suspend fun isStillReferenced(id: Int) = dao.isStillReferenced(id)
+    suspend fun isStillReferenced(id: Int): Boolean
 }
+
