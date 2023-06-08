c1633af5261d3bbfea9348cebe7134bcb389364f
Diego Beraldin <diego.beraldin@gmail.com>
2023-06-08 13:40:40 +0200

    refactor(spellcheck): migration to languagetool

--- domain-spellcheck/src/jvmMain/kotlin/spellcheck/spelling/DefaultSpelling.kt
+++ domain-spellcheck/src/jvmMain/kotlin/spellcheck/spelling/DefaultSpelling.kt
@@ -1,62 +1,82 @@
 package spellcheck.spelling
 
-import pt.tumba.spell.DefaultWordFinder
-import pt.tumba.spell.SpellChecker
-import spellcheck.repo.DefaultSpellCheckRepository
-import java.io.InputStreamReader
-import java.io.Reader
+import org.languagetool.JLanguageTool
+import org.languagetool.Language
+import org.languagetool.language.AmericanEnglish
+import org.languagetool.language.Arabic
+import org.languagetool.language.Catalan
+import org.languagetool.language.Chinese
+import org.languagetool.language.Dutch
+import org.languagetool.language.Esperanto
+import org.languagetool.language.French
+import org.languagetool.language.Galician
+import org.languagetool.language.GermanyGerman
+import org.languagetool.language.Greek
+import org.languagetool.language.Irish
+import org.languagetool.language.Italian
+import org.languagetool.language.Japanese
+import org.languagetool.language.Persian
+import org.languagetool.language.Polish
+import org.languagetool.language.Portuguese
+import org.languagetool.language.Romanian
+import org.languagetool.language.Russian
+import org.languagetool.language.Slovak
+import org.languagetool.language.Spanish
+import org.languagetool.language.Ukrainian
 
 class DefaultSpelling : Spelling {
 
     companion object {
-        private val SUPPORTED_LANGUAGES = listOf("en", "es", "it", "pt")
-
-        fun String.getDictionary(): Reader? = DefaultSpellCheckRepository::class.java.classLoader
-            .getResourceAsStream("dictionaries/$this.txt")
-            ?.let { InputStreamReader(it) }
-    }
-
-    override var isInitialized = false
-
-    private val spellChecker = SpellChecker()
-
-    override fun setLanguage(code: String) {
-        if (code in SUPPORTED_LANGUAGES) {
-            runCatching {
-                val reader = code.getDictionary()
-                if (reader != null) {
-                    spellChecker.initialize(reader)
-                    isInitialized = true
-                }
-            }
-        } else {
-            isInitialized = false
+        private fun String.toLanguage(): Language? = when (this) {
+            "en" -> AmericanEnglish()
+            "pt" -> Portuguese()
+            "ar" -> Arabic()
+            "nl" -> Dutch()
+            "ca" -> Catalan()
+            "zh" -> Chinese()
+            "eo" -> Esperanto()
+            "fr" -> French()
+            "gl" -> Galician()
+            "de" -> GermanyGerman()
+            "el" -> Greek()
+            "ga" -> Irish()
+            "it" -> Italian()
+            "ja" -> Japanese()
+            "fa" -> Persian()
+            "pl" -> Polish()
+            "ro" -> Romanian()
+            "ru" -> Russian()
+            "sk" -> Slovak()
+            "es" -> Spanish()
+            "uk" -> Ukrainian()
+            else -> null
         }
     }
 
+    override val isInitialized: Boolean
+        get() = ::spellChecker.isInitialized
+
+    private lateinit var spellChecker: JLanguageTool
+
+    override fun setLanguage(code: String) {
+        spellChecker = JLanguageTool(code.toLanguage())
+    }
+
     override fun check(word: String): Pair<Boolean, List<String>> {
-        val finder = DefaultWordFinder(word)
-        val aux = finder.next() ?: return true to emptyList()
-        val aux2 = spellChecker.findMostSimilar(aux)
-        return when {
-            aux2 == null -> {
-                // misspelled word with no suggestions
-                false to emptyList()
+        val defaultResult: Pair<Boolean, List<String>> = true to emptyList()
+        if (!isInitialized) return defaultResult
+
+        try {
+            val matches = spellChecker.check(word)
+            val suggestions = matches.flatMap { it.suggestedReplacements }
+            if (suggestions.isEmpty()) {
+                return defaultResult
             }
 
-            !aux2.equals(aux, ignoreCase = true) -> {
-                // misspelled word with suggestions
-                val suggestions = spellChecker.findMostSimilarList(aux)
-                false to buildList {
-                    for (e in suggestions) {
-                        (e as? String)?.also {
-                            this += it
-                        }
-                    }
-                }
-            }
-
-            else -> true to emptyList() // correct word
+            return false to suggestions
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            return defaultResult
         }
     }
 }
