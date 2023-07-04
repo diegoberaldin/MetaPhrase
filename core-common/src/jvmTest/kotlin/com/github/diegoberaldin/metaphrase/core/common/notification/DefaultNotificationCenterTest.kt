bd3f83536340ad10d49d79ab09e5a341161682f3
Diego Beraldin <diego.beraldin@gmail.com>
2023-07-04 23:29:20 +0200

    chore(core-common): fix flaky test (#110)

--- core-common/src/jvmTest/kotlin/com/github/diegoberaldin/metaphrase/core/common/notification/DefaultNotificationCenterTest.kt
+++ core-common/src/jvmTest/kotlin/com/github/diegoberaldin/metaphrase/core/common/notification/DefaultNotificationCenterTest.kt
@@ -1,30 +1,19 @@
 package com.github.diegoberaldin.metaphrase.core.common.notification
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.runBlocking
+import app.cash.turbine.test
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class DefaultNotificationCenterTest {
-
     private val sut = DefaultNotificationCenter
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
     @Test
-    fun givenNotificationCenterWhenShowProgressEventSentThenItIsReceived() {
-        var evt: NotificationCenter.Event? = null
-        sut.events.onEach {
-            evt = it
-        }.launchIn(scope)
-        runBlocking {
+    fun givenNotificationCenterWhenShowProgressEventSentThenItIsReceived() = runTest {
+        sut.events.test {
             sut.send(NotificationCenter.Event.ShowProgress(true))
-            delay(100)
+            val evt = awaitItem()
             assertNotNull(evt)
             assertEquals(NotificationCenter.Event.ShowProgress(true), evt)
         }
