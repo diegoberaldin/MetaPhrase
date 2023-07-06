49da465b322a28fa99f83257abc93dca58c92a8b
Diego Beraldin <diego.beraldin@gmail.com>
2023-07-06 11:27:47 +0200

    chore(architecture): migrate intro component

--- feature-intro/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/feature/intro/presentation/IntroComponent.kt
+++ feature-intro/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/feature/intro/presentation/IntroComponent.kt
@@ -1,6 +1,23 @@
 package com.github.diegoberaldin.metaphrase.feature.intro.presentation
 
+import com.github.diegoberaldin.metaphrase.core.common.architecture.MviModel
+
 /**
- * Intro component.
+ * Intro component contract.
  */
-interface IntroComponent
+interface IntroComponent : MviModel<IntroComponent.ViewIntent, IntroComponent.UiState, IntroComponent.Effect> {
+    /**
+     * View intents.
+     */
+    sealed interface ViewIntent
+
+    /**
+     * Effects.
+     */
+    sealed interface Effect
+
+    /**
+     * UI state.
+     */
+    object UiState
+}
