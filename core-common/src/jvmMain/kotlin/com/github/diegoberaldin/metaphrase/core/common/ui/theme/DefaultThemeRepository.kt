17fe620571bad47b680dd8fd5dd8148e7bb312b1
Diego Beraldin <diego.beraldin@gmail.com>
2023-07-09 23:04:18 +0200

    chore(core-common): change palette and refactoring (#117)
    * chore(core-common): change palette
    
    * chore(newproject): organize imports
    
    * refactor(core-common): allow to change UI font family

--- core-common/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/core/common/ui/theme/DefaultThemeRepository.kt
+++ core-common/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/core/common/ui/theme/DefaultThemeRepository.kt
@@ -1,11 +1,19 @@
 package com.github.diegoberaldin.metaphrase.core.common.ui.theme
 
+import androidx.compose.ui.text.font.FontFamily
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class DefaultThemeRepository : ThemeRepository {
+
     override val theme = MutableStateFlow<ThemeState>(ThemeState.Light)
+
+    override val fontFamily = MutableStateFlow(AppFontFamily)
 
     override fun changeTheme(value: ThemeState) {
         theme.value = value
+    }
+
+    override fun changeFontFamily(family: FontFamily) {
+        fontFamily.value = family
     }
 }
--- core-common/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/core/common/ui/theme/ThemeRepository.kt
+++ core-common/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/core/common/ui/theme/ThemeRepository.kt
@@ -1,5 +1,6 @@
 package com.github.diegoberaldin.metaphrase.core.common.ui.theme
 
+import androidx.compose.ui.text.font.FontFamily
 import kotlinx.coroutines.flow.StateFlow
 
 /**
@@ -27,9 +28,21 @@ interface ThemeRepository {
     val theme: StateFlow<ThemeState>
 
     /**
+     * Observable font family.
+     */
+    val fontFamily: StateFlow<FontFamily>
+
+    /**
      * Change the application UI theme.
      *
      * @param value Value to set
      */
     fun changeTheme(value: ThemeState)
+
+    /**
+     * Changethe  font family for the application UI.
+     *
+     * @param family Font Family to use
+     */
+    fun changeFontFamily(family: FontFamily)
 }
