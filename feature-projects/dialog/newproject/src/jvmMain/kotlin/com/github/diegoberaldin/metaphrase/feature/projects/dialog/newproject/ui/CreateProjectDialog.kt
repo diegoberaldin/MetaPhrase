01fc350fdaf5d3078732dff417063462241227d0
Diego Beraldin <diego.beraldin@gmail.com>
2023-07-09 22:44:58 +0200

    chore(newproject): organize imports

--- feature-projects/dialog/newproject/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/feature/projects/dialog/newproject/ui/CreateProjectDialog.kt
+++ feature-projects/dialog/newproject/src/jvmMain/kotlin/com/github/diegoberaldin/metaphrase/feature/projects/dialog/newproject/ui/CreateProjectDialog.kt
@@ -17,13 +17,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.onClick
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
